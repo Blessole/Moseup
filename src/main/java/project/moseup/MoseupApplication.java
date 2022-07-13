7c35ca5c617dbda90c5d87e0e8611491945a7f91
DESKTOP-NLFOGE7\kwon <enadlekdq@gmail.com>
2022-07-13 15:38:30 +0900

    initialize repository for main branch

---
+++ src/main/java/project/moseup/MoseupApplication.java
@@ -0,0 +1,13 @@
+package project.moseup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoseupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoseupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/moseup/MoseupApplicationTests.java
@@ -0,0 +1,13 @@
+package project.moseup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoseupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
