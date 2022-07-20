de50e9b69a18027714af220b5a069a36fee49874
DESKTOP-NLFOGE7\kwon <enadlekdq@gmail.com>
2022-07-20 17:13:39 +0900

    [Feat] 팀 메인 페이지, 문의 페이지, 인증 게시판 생성에 따른 teamController 생성

---
+++ src/main/java/project/moseup/controller/TeamPageController.java
@@ -0,0 +1,29 @@
+package project.moseup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import project.moseup.service.TeamService;
+
+@Controller
+@RequiredArgsConstructor
+public class TeamPageController {
+
+	private final TeamService teamService;
+	
+	@GetMapping("teamPage")
+	public String teamMainPage() {
+		return "teams/teamMain";
+	}
+	
+	@GetMapping("teamAskBoard")
+	public String teamAskBoardPage() {
+		return "teams/teamAskBoard";
+	}
+	
+	@GetMapping("teamCheckBoard")
+	public String teamCheckBoardPage() {
+		return "teams/teamCheckBoard";
+	}
+}
