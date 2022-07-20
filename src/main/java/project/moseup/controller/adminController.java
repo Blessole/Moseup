11e9d057d7f5e55d88ee367bc0d03ca1f6c25da4
ftfg777 <4245093@naver.com>
2022-07-21 02:25:08 +0900

    관리자 사이드메뉴 & 헤드 테스트

--- src/main/java/project/moseup/controller/adminController.java
+++ src/main/java/project/moseup/controller/adminController.java
@@ -1,15 +1,18 @@
 package project.moseup.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("admin")
 public class adminController {
 
     @GetMapping("/hello")
-    public String admin(){
+    public String admin(@RequestParam(name="name", required = false, defaultValue = "JeongChanWoo")String name, Model model){
+        model.addAttribute("name", name);
         return "admin/adminSidebar";
     }
 }
