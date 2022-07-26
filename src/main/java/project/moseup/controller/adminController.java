4bceadc33b2d47fc60660050a966c51023901a41
JeongChanWoo <ftfgz5093@gmail.com>
2022-07-27 00:18:38 +0900

    회원디테일 폼 , 회원가입 폼 완성

--- src/main/java/project/moseup/controller/adminController.java
+++ src/main/java/project/moseup/controller/adminController.java
@@ -3,13 +3,17 @@ package project.moseup.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import project.moseup.domain.DeleteStatus;
 import project.moseup.domain.Member;
+import project.moseup.domain.MemberGender;
 import project.moseup.repository.admin.AdminMemberRepository;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("admin")
@@ -33,8 +37,39 @@ public class adminController {
     }
 
     @GetMapping("/memberAddForm")
-    public String memberAdd(){
+    public String memberForm(Model model){
+        model.addAttribute("member", new Member());
+        model.addAttribute("male", MemberGender.MALE);
+        model.addAttribute("female", MemberGender.FEMALE);
+        model.addAttribute("false", DeleteStatus.FALSE);
         return "admin/memberAddForm";
     }
+
+    @PostMapping("/memberAddForm")
+    public String memberSubmit(@ModelAttribute Member member, @RequestParam(required = false) MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+
+        String savePath = System.getProperty("user.dir")+"\\D:\\moseupFile";
+
+
+
+
+
+        member.setPhoto(originalFilename);
+        member.setMemberDate(LocalDateTime.now());
+
+        adminMemberRepository.save(member);
+
+        return "redirect:/admin/memberList";
+    }
+
+    @GetMapping("/memberList")
+    public String memberDetail(Model model, @RequestParam Long mno){
+        Optional<Member> member = adminMemberRepository.findById(mno);
+        model.addAttribute("member", member);
+        return "admin/memberDetail";
+    }
+
+
 
 }
