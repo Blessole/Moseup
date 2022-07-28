d163a74a43d557a95090e9ecdb8bdbcb05ce1b09
Sol Kim <101856058+Blessole@users.noreply.github.com>
2022-07-28 17:35:04 +0900

    [Refactor] 병합 후 오류나는 부분 정리

--- src/main/java/project/moseup/config/SecurityConfig.java
+++ src/main/java/project/moseup/config/SecurityConfig.java
@@ -40,8 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/", "/main", "/main/**", "/members/**").permitAll()
+                .antMatchers("/", "/admin/**", "/templates/admin", "/teams/**", "/main", "/main/**", "/members/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/project/moseup/controller/TeamPageController.java
+++ src/main/java/project/moseup/controller/TeamPageController.java
@@ -6,16 +6,18 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import project.moseup.domain.TeamAskBoard;
+import project.moseup.dto.TeamAskForm;
 import project.moseup.service.TeamAskBoardService;
 import project.moseup.service.TeamService;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/teams/")
 public class TeamPageController {
 
 	private final TeamService teamService;
