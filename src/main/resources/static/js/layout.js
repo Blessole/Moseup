11bb979d3f0f351b261271f9efa3471c9de9abea
Sol Kim <101856058+Blessole@users.noreply.github.com>
2022-09-16 23:02:00 +0900

    [Refactor] 네비바 Search 바 스타일 변경, 마이페이지 인증글, 팀리스트 아이콘 및 CSS 부분 수정, 로고 & 파비콘 적용

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -1,17 +1,56 @@
 /*네비바 submit체크*/
 function chk() {
-		var text = $('#search').val();
-		var filter2 = $('#filter2').val();
-		
+		let text = $('#search').val();
+		let filter2 = $('#filter2').val();
+
 		if(text == "" || text == null) {
 			return false;
 		}
-		
+
 		if (filter2 == "" || filter2 == null) {
 			$('#filter2').val("nothing");
 		}
-		
+
 		if ($('.filter1value').is(":checked") == false) {
 			$('#filter1').val("nothing");
 		}
 	}
+
+// /*네비바 submit체크 (by 솔) */
+function navChk() {
+	let navText = $('#search').val();
+
+	if(navText == "" || navText == null) {
+		return false;
+	} else {
+		$('#filter2').val("nothing");
+		$('#filter1').val("nothing");
+	}
+	}
+
+/* 네비바 Search by 솔 */
+const search = document.querySelector(".nav-search");
+const btn = document.querySelector(".searchBtn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+	search.classList.toggle("active");
+	input.focus();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
